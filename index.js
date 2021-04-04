e11993ff14588ab573dd97238ed0921e3a04b556
aiden-bluepony <71100818+aiden-bluepony@users.noreply.github.com>
2021-04-03 23:58:37 -0400

    extracted gql module

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const {ApolloServer, gql} = require('apollo-server')
+const {ApolloServer} = require('apollo-server')
 const { GraphQLScalarType } = require('graphql')
 const { Kind } = require('graphql/language')
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-// Next Steps
-// 1. modularize code
+const {typeDefs, resolvers} = require('./gql');
+
+
 
 mongoose.connect(
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vkt8n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
@@ -22,70 +23,6 @@ const mongoProductSchema = new mongoose.Schema({
 });
 // ------ mongo model
 const Product = mongoose.model('Product', mongoProductSchema);
-
-// ----------------------------- GraphQl Schema
-const typeDefs = gql`
-  type Product {
-    id: ID
-    name: String
-    price: Float
-    img: String
-  }
-  type Query {
-    product(id: ID): Product
-    products: [Product]
-  }
-  input ProductInput {
-    id: ID
-    name: String
-    rating: Float
-    img: String
-  }
-  type Mutation {
-    addProduct(product: ProductInput): [Product]
-  }
-`;
-
-
-// ---------------------------------- Resolvers (Query Responders)
-const resolvers = {
-  Query: {
-    products: async () => {
-      try{
-        const allProducts = await Product.find();
-        return allProducts;
-      } catch(err){
-        console.log('err', err);
-        return[]
-      }
-    },
-    product: async (obj, args, context, info) => {
-      try{
-        const foundProduct = await Product.findById(args.id)
-        return foundProduct
-      }catch(err){
-        console.log('err', err);
-        return {}
-      }
-    }
-  },
-  Mutation: {
-    addProduct: async (obj, {product}, context, info) => {
-      try {
-        await Product.create({
-          ...product
-        })
-        const allProducts = await Product.find();
-        return allProducts
-      }catch(err){
-        console.log('error', err);
-      }
-    }
-  }
-}
-
-
-
 
 // ---------------------------------- Apollo Server
 const server = new ApolloServer({
