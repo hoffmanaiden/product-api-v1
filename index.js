1513465270a689c758cc1b12be0bbd67785abad3
aiden-bluepony <71100818+aiden-bluepony@users.noreply.github.com>
2021-04-05 20:44:59 -0400

    CR working with DB, still needs UD

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const {ApolloServer} = require('apollo-server')
+const {ApolloServer, gql} = require('apollo-server')
 const { GraphQLScalarType } = require('graphql')
 const { Kind } = require('graphql/language')
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-const {typeDefs, resolvers} = require('./gql');
-
+// const {typeDefs, resolvers} = require('./gql');
 
 
 mongoose.connect(
@@ -23,6 +22,69 @@ const mongoProductSchema = new mongoose.Schema({
 });
 // ------ mongo model
 const Product = mongoose.model('Product', mongoProductSchema);
+
+
+// ----------------------------- GraphQl Schema
+const typeDefs = gql`
+  type Product {
+    id: ID
+    name: String
+    price: Float
+    img: String
+  }
+  type Query {
+    product(id: ID): Product
+    products: [Product]
+  }
+  input ProductInput {
+    id: ID
+    name: String
+    price: Float
+    img: String
+  }
+  type Mutation {
+    addProduct(product: ProductInput): [Product]
+  }
+`;
+
+
+
+// ---------------------------------- Resolvers (Query Responders)
+const resolvers = {
+  Query: {
+    products: async () => {
+      try{
+        const allProducts = await Product.find();
+        return allProducts;
+      } catch(err){
+        console.log('err', err);
+        return[]
+      }
+    },
+    product: async (obj, args, context, info) => {
+      try{
+        const foundProduct = await Product.findById(args.id)
+        return foundProduct
+      }catch(err){
+        console.log('err', err);
+        return {}
+      }
+    }
+  },
+  Mutation: {
+    addProduct: async (obj, {product}, context, info) => {
+      try {
+        await Product.create({
+          ...product
+        })
+        const allProducts = await Product.find();
+        return allProducts
+      }catch(err){
+        console.log('error', err);
+      }
+    }
+  }
+}
 
 // ---------------------------------- Apollo Server
 const server = new ApolloServer({
@@ -47,3 +109,6 @@ db.once('open', function () {
     port: process.env.PORT || 4000
   }).then(({url}) => { console.log(`Server running on ${url}`)})  
 })
+
+// Send DB Object to --> gql for CRUD
+module.exports = {Product}
